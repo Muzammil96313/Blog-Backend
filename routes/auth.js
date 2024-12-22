10d5691b5c7db3ef4c3ac46fe2786b5d096da590
Muzammil96313 <muzammil.hassan.dev7@gmail.com>
2024-12-22 18:26:23 +0500

    new changes

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,8 @@ const User = require("../models/User");
 const authenticate = require("../middleware/authenticate");
 const multer = require("multer");
 const path = require("path");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 const router = express.Router();
 
@@ -19,16 +21,20 @@ router.get("/profile", authenticate, async (req, res) => {
   }
 });
 
-// Configure multer for storing uploaded files
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./uploads/avatars");
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, `${uniqueSuffix}${path.extname(file.originalname)}`);
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "avatars", // Folder in Cloudinary
+    allowed_formats: ["jpg", "jpeg", "png"],
   },
 });
+
 const upload = multer({ storage });
 
 router.put(
