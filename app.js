8de13b5077dc22d52457bfd5d4d6bf19db7a2f8a
Muzammil96313 <muzammil.hassan.dev7@gmail.com>
2024-12-22 12:21:56 +0500

    error fix

--- app.js
+++ app.js
@@ -33,5 +33,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app; // Ensure the app is exported
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
